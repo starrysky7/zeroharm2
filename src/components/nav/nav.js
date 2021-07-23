129481c33f53bc67eff97671346bf02dd1c96974
696e66696e697479 <paulb@live.in>
2021-07-23 12:51:35 +0530

    latest update

---
+++ src/components/nav/nav.js
@@ -0,0 +1,22 @@
+import './nav.css';
+
+function Nav() {
+  return (
+    <div className="nav">
+        <div className="nav-logo-container">
+                <span className="nav-logo-top">ZEROHARM</span>
+                <span className="nav-logo-bottom">SCIENCES</span>
+            </div>
+            <div class="nav-items-container">
+                <a href="#" className="nav-item">Origins</a>
+                <a href="#" className="nav-item">Process</a>
+                <a href="#" className="nav-item">Products</a>
+                <a href="#" className="nav-item">Validation</a>
+                <a href="#" className="nav-item">Beliefs</a>
+                <a href="#" className="nav-item">Contact Us</a>
+            </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/pages/landing/landing.js
@@ -0,0 +1,12 @@
+import Nav from '../../components/nav/nav';
+import './landing.css';
+
+function Landing() {
+  return (
+    <div className="landing">
+        <Nav />
+    </div>
+  );
+}
+
+export default Landing;
