00170214cf6cf669f1639688c8bb8d45780dfff8
696e66696e697479 <paulb@live.in>
2021-08-02 16:03:43 +0530

    changed order

--- src/views/landing/sectionEight/sectionEight.js
+++ src/views/landing/sectionEight/sectionEight.js
@@ -4,8 +4,8 @@ import * as ReactBootStrap from "react-bootstrap";
 
 function sectionEight() {
   return (
-    <div className="section" >
-      <ReactBootStrap.Row>
+    <div className="section section8" >
+      <ReactBootStrap.Row className="desktop">
 
         <ReactBootStrap.Col xs={12} md={5}>
           <div className="content-container section8-content-container">
@@ -22,9 +22,35 @@ function sectionEight() {
           </div>
         </ReactBootStrap.Col>
 
+        <ReactBootStrap.Col xs={12} md={7}>
+          <div >
+            <img className="machineImage" src="in test we trust.png" />
+          </div>
+        </ReactBootStrap.Col>
+
+      </ReactBootStrap.Row>
+
+
+      <ReactBootStrap.Row className="mobile">
+
         <ReactBootStrap.Col xs={12} md={7} >
           <div >
             <img className="machineImage" src="in test we trust.png" />
+          </div>
+        </ReactBootStrap.Col>
+
+        <ReactBootStrap.Col xs={12} md={5}>
+          <div className="content-container section8-content-container">
+            <span className="title-text green">IN TEST WE TRUST.</span>
+            <span className="default-text grey">We are at the intersection of nature and efcacy.
+            The tests are at the highest levels at Laboratories
+    of international repute. <br /><br />
+
+    A promise that lives up even in the generics range.
+    For instance the Laboratory testing showed
+    serum levels by more than ve times with our
+    nano-capsulated generics over conventional generics.</span>
+            <a href="/test" class="default-button green">KNOW MORE</a>
           </div>
         </ReactBootStrap.Col>
 
--- src/views/landing/sectionSix/sectionSix.js
+++ src/views/landing/sectionSix/sectionSix.js
@@ -4,7 +4,7 @@ import * as ReactBootStrap from "react-bootstrap";
 function sectionSix() {
   return (
     <div className="section section6">
-      <ReactBootStrap.Row>
+      <ReactBootStrap.Row className="desktop">
 
         <ReactBootStrap.Col xs={6} md={6}>
           <div className="content-container section6-content-container">
@@ -13,7 +13,24 @@ function sectionSix() {
             <a href="#" class="default-button white">KNOW MORE</a>
           </div>
         </ReactBootStrap.Col>
-      
+
+      </ReactBootStrap.Row>
+
+      <ReactBootStrap.Row className="mobile">
+
+        <ReactBootStrap.Col xs={6} md={6}>
+          <div className="content-container section6-content-container">
+            <span class="title-text white">OWNING TRACEABILITY</span>
+            <span class="default-text white">With ownership of the entire valuechain<br />we document, record and identify the<br />product right through. From seed to<br />consumption, there is complete<br />traceability</span>
+            <a href="#" class="default-button white">KNOW MORE</a>
+          </div>
+        </ReactBootStrap.Col>
+        <ReactBootStrap.Col xs={6} md={6}>
+          <div>
+            <img src="/phone-min.gif" className="section6-image"/>
+          </div>
+        </ReactBootStrap.Col>
+
       </ReactBootStrap.Row>
 
     </div>
