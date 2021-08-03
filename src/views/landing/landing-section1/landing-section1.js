26e3b02a4a585ac713689077c2c7d9644eb495cc
696e66696e697479 <paulb@live.in>
2021-08-03 11:37:34 +0530

    aa

--- src/views/landing/landing-section1/landing-section1.js
+++ src/views/landing/landing-section1/landing-section1.js
@@ -4,12 +4,12 @@ import * as ReactBootStrap from "react-bootstrap";
 function LandingSection1() {
   return (
     <div className="section section1">
-      <ReactBootStrap.Row>
-        
+      <ReactBootStrap.Row className="desktop">
+
         <ReactBootStrap.Col sm={3} md={3} xs={12}>
-      <div>
-          <span className="landing-section1-text1 title-text green">WHERE IT <br /> BEGINS.</span>
-       </div>
+          <div>
+            <span className="landing-section1-text1 title-text green">WHERE IT <br /> BEGINS.</span>
+          </div>
         </ReactBootStrap.Col>
 
         <ReactBootStrap.Col sm={6} md={6} xs={12}>
@@ -19,14 +19,40 @@ function LandingSection1() {
             <img className="leaf-img" src="LEAFGIF2small.gif" />
           </div>
         </ReactBootStrap.Col>
-        
+
         <ReactBootStrap.Col sm={3} md={3}  >
-      <div>
-          <span className="landing-section1-text2 default-text grey align-right">Ensure lasting health and well-being of humanity by pre-empting diseases and offering preventive, precision, natural medication.</span>
-      </div>
+          <div>
+            <span className="landing-section1-text2 default-text grey align-right">Ensure lasting health and well-being of humanity by pre-empting diseases and offering preventive, precision, natural medication.</span>
+          </div>
         </ReactBootStrap.Col>
 
       </ReactBootStrap.Row>
+
+
+      <ReactBootStrap.Row className="mobile">
+
+        <ReactBootStrap.Col sm={3} md={3} xs={12}>
+          <div>
+            <span className="landing-section1-text1 title-text green">WHERE IT <br /> BEGINS.</span>
+          </div>
+        </ReactBootStrap.Col>
+
+        <ReactBootStrap.Col sm={3} md={3}  >
+          <div>
+            <span className="landing-section1-text2 default-text grey align-right">Ensure lasting health and well-being of humanity by pre-empting diseases and offering preventive, precision, natural medication.</span>
+          </div>
+        </ReactBootStrap.Col>
+
+        <ReactBootStrap.Col sm={6} md={6} xs={12}>
+          <div className="leaf-anim-container">
+            {/* <img className="leaf-static-img" src="leaf final.png" />
+            <img className="leaf-anim-img" src="leaf final.png" /> */}
+            <img className="leaf-img" src="LEAFGIF2small.gif" />
+          </div>
+        </ReactBootStrap.Col>
+
+      </ReactBootStrap.Row>
+
 
     </div>
   );
