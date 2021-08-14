003246e46efd6cc1af7dc2a76d16ad89a298c8d5
696e66696e697479 <paulb@live.in>
2021-08-14 14:06:49 +0530

    Update about-section4.js

--- src/views/about-section4/about-section4.js
+++ src/views/about-section4/about-section4.js
@@ -7,30 +7,30 @@ function AboutSection4() {
     <div className="section" >
       <ReactBootStrap.Row>
 
-<ReactBootStrap.Col xs={12} md={6}>
-  <div className="content-container about-section4-content-container">
-    <span className="default-text grey">
-    <b>How different is this from the
-conventional method?</b><br/><br/>
+        <ReactBootStrap.Col xs={12} md={6}>
+          <div className="content-container about-section4-content-container">
+            <span className="default-text grey">
+              <b>How different is this from the
+conventional method?</b><br /><br />
 The existing solution defeats its reason for
-existence as it evaporates some of the more potent 
-but volatile compounds and leaves behind harmful 
-chemical residues.<br/><br/>
+existence as it evaporates some of the more potent
+but volatile compounds and leaves behind harmful
+chemical residues.<br /><br />
 Aqueous extraction involves a higher outlay
 which results in the purest and safest extracts.
 The potent compounds remain
 ensuring a higher efcacy, and there are
 no chemical residues. </span>
-  </div>
-</ReactBootStrap.Col>
+          </div>
+        </ReactBootStrap.Col>
 
-<ReactBootStrap.Col xs={12} md={6} >
-  <div >
-    <img className="about-section6-image" src="machine.jpg" />
-  </div>
-</ReactBootStrap.Col>
+        <ReactBootStrap.Col xs={12} md={6} >
+          <div >
+            <img className="about-section6-image" src="machine.jpg" />
+          </div>
+        </ReactBootStrap.Col>
 
-</ReactBootStrap.Row>
+      </ReactBootStrap.Row>
 
     </div>
   );
